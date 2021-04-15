aa70567a70c10c66212058b3a169f1374a3c7595
jev0m <69557200+jev0m@users.noreply.github.com>
2021-04-15 04:22:27 +0300

    Add files via upload

---
+++ Group.py
@@ -0,0 +1,91 @@
+import sys as n
+import time as mm
+from distutils.command import clean
+
+def slow(M):
+	for c in M + '\n':
+		n.stdout.write(c)
+		n.stdout.flush()
+		mm.sleep(10. / 150)
+
+green_color = "\033[1;93m"
+C = "\033[0m"
+W = "\033[96m"
+R="\033[1;31m"
+G="\033[0;36m"
+Y="\033[0;33m"
+
+
+print(Y+'(Rahman-Lawen-Rahand-Ahmad-Qara-Ranj-Hama)')
+ask = input(R+"\nDatawe Che Bnase : ")
+
+
+
+
+if ask == 'lawen':
+    slow(G+"""\nLawen Nasrawa Ba Lawen Soydery Xawne Awaya Rdenkey Pre Habe Tamne 74 3aqre 2
+───▄▀▀▀▀▀───▄█▀▀▀█▄
+──▐▄▄▄▄▄▄▄▄██▌▀▄▀▐██
+──▐▒▒▒▒▒▒▒▒███▌▀▐███
+───▌▒▓▒▒▒▒▓▒██▌▀▐██
+───▌▓▐▀▀▀▀▌▓─  ▀▀""")
+
+
+if ask == 'qara':
+    slow(G+"""\nNasrwa Ba Matuid Xawne Awaya Bbta La 3ebek Baqwat Taman 140 3aqr 13
+───▄▄▄
+─▄▀░▄░▀▄
+─█░█▄▀░█
+─█░▀▄▄▀█▄█▄▀
+▄▄█▄▄▄▄███▀""")
+
+
+if ask == 'ranj':
+    slow(G+"""\nNasrawa Ba 3ashq Ba Kere Xawne Awaya Hama Ba Rwte Bbine Taman 3 3aqre 40 
+───▄██▄─────────────▄▄
+──█████▄▄▄▄───────▄▀
+────▀██▀▀████▄───▄▀
+───▄█▀▄██▄████▄─▄█
+▄▄█▀▄▄█─▀████▀██▀
+
+    """)
+
+if ask == 'hama':
+    slow(G+"""\nNasrawa Ba Kare Gawra Xawne Awaya Xerate Payda Ka Taman 0 3aqre 0
+────██──────▀▀▀██
+──▄▀█▄▄▄─────▄▀█▄▄▄
+▄▀──█▄▄──────█─█▄▄
+─▄▄▄▀──▀▄───▄▄▄▀──▀▄
+─▀───────▀▀─▀───────▀▀
+
+""")
+
+if ask == 'rahand':
+    slow(G+"""\nNasrawa Ba Sha Qozaka Xawne Awaya Dast La Lawen Warda Taman 9999999 3aqr 9999999 Chbkam Kabra hacke krdya
+ ▄█▀█▄
+▄████████▄
+██▀▼▼▼▼▼
+█████▄▲▲▲
+██████▀▀▀▀
+
+    """)
+
+if ask == 'ahmad':
+    slow(G+"""\nNasrawa ERORR Xawne ERORR Taman ERORR 3aqr ERORR
+─────█─▄▀█──█▀▄─█─────
+────▐▌──────────▐▌────
+────█▌▀▄──▄▄──▄▀▐█────
+───▐██──▀▀──▀▀──██▌───
+──▄████▄──▐▌──▄████▄──
+
+    """)
+
+if ask =='rahman':
+    slow('\nNasrawa Kaka Xawne Awaya juventos championse bba taman 28 3aqr 9999999')
+
+
+slow(Y+f'\nWamzane Radioya {ask} Basheke La groupakaman {ask} Xoshawesty')
+
+slow(R+'\nMade By AHMAD Fkray Rahand')
+
+slow('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
